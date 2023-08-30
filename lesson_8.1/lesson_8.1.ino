5f8bf7da9c3108de603430f693786e0ca1653c6c
Lucas A <lucas.p00p.emoji@gmail.com>
2023-08-30 17:32:35 +1000

    I made the motors speed faster and changed the pins

--- lesson_8.1/lesson_8.1.ino
+++ lesson_8.1/lesson_8.1.ino
@@ -8,7 +8,10 @@
 #define ML_Ctrl 13  //define the direction control pin of left motor
 #define ML_PWM 11   //define the PWM control pin of left motor
 #define MR_Ctrl 12  //define direction control pin of right motor
-#define MR_PWM 3   // define the PWM control pin of right motor
+
+#define ML_Ctrl 13  //define the direction control pin of left motor
+#define MR_PWM 11   //define the PWM control pin of left motor
+#define MR_Ctrl 12  //define direction control pin of right motor
 
 void setup()
 {
@@ -21,36 +24,36 @@ void setup()
 void loop()
 { 
   digitalWrite(ML_Ctrl,LOW);//set the direction control pin of left motor to LOW
-  analogWrite(ML_PWM,200);//set the PWM control speed of left motor to 200
+  analogWrite(ML_PWM,200);//set the PWM control speed of left motor to 2000
   digitalWrite(MR_Ctrl,LOW);//set the direction control pin of right motor to LOW
-  analogWrite(MR_PWM,200);//set the PWM control speed of right motor to 200
+  analogWrite(MR_PWM,200);//set the PWM control speed of right motor to 2000
 
   //front
-  delay(2000);//delay in 2s
+  delay(1000);//delay in 2s
    digitalWrite(ML_Ctrl,HIGH);//set the direction control pin of left motor to HIGH
-  analogWrite(ML_PWM,200);//set the PWM control speed of left motor to 200  
+  analogWrite(ML_PWM,2000);//set the PWM control speed of left motor to 200
 digitalWrite(MR_Ctrl,HIGH);//set the direction control pin of right motor to HIGH
-  analogWrite(MR_PWM,200);//set the PWM control speed of right motor to 200
+  analogWrite(MR_PWM,2000);//set the PWM control speed of right motor to 2000
 
    //back
-  delay(2000);//delay in 2s 
+  delay(1000);//delay in 2s 
   digitalWrite(ML_Ctrl,HIGH);//set the direction control pin of left motor to HIGH
-  analogWrite(ML_PWM,200);//set the PWM control speed of left motor to 200
+  analogWrite(ML_PWM,2000);//set the PWM control speed of left motor to 2000
   digitalWrite(MR_Ctrl,LOW);//set the direction control pin of right motor to LOW
-  analogWrite(MR_PWM,200);//set the PWM control speed of right motor to 200
+  analogWrite(MR_PWM,2000);//set the PWM control speed of right motor to 2000
 
     //left
-  delay(2000);//delay in 2s
+  delay(1000);//delay in 2s
    digitalWrite(ML_Ctrl,LOW);//set the direction control pin of left motor to LOW
-  analogWrite(ML_PWM,200);//set the PWM control speed of left motor to 200
+  analogWrite(ML_PWM,2000);//set the PWM control speed of left motor to 2000
   digitalWrite(MR_Ctrl,HIGH);//set the direction control pin of right motor to HIGH
-  analogWrite(MR_PWM,200);//set the PWM control speed of right motor to 200
+  analogWrite(MR_PWM,2000);//set the PWM control speed of right motor to 2000
 
    //right
-  delay(2000);//delay in 2s
-  analogWrite(ML_PWM,0);//set the PWM control speed of left motor to 0
-  analogWrite(MR_PWM,0);//set the PWM control speed of right motor to 0
+  delay(1000);//delay in 2s
+  analogWrite(ML_PWM,2000);//set the PWM control speed of left motor to 2000
+  analogWrite(MR_PWM,2000);//set the PWM control speed of right motor to 2000
 
     //stop
-  delay(2000);//delay in 2s
+  delay(1000);//delay in 2s
 }//*****************************************
